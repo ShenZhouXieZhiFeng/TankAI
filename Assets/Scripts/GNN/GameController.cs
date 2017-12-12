790974cb93052944d838a81130ad87aef0ca50a5
Stone <827443469@qq.com>
2017-12-12 21:54:13 +0800

    Gnn

---
+++ Assets/Scripts/GNN/GameController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public float PhysicFrameTime = 0.01f;
+    public bool StopPhysic = false;
+
+    private float physicSteps = 0.01f;
+
+	void Start () {
+        StartCoroutine(phySimulate());
+    }
+	
+    IEnumerator phySimulate() {
+        while (!StopPhysic) {
+            Physics.Simulate(physicSteps);
+            yield return new WaitForSeconds(PhysicFrameTime);
+        }
+        yield return 0;
+    }
+}
---
+++ Assets/Scripts/GNN/Tank.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tank : MonoBehaviour {
+
+    public float maxSpeed;
+    public float maxTorque;
+
+    public float shootCoolDown;
+    public Transform shootPoint;
+    public GameController bullet;
+
+    Rigidbody m_rigidbody;
+
+	void Start () {
+        m_rigidbody = GetComponent<Rigidbody>();
+    }
+	
+	void Update () {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        SetMove(v);
+        SetRotate(h);
+	}
+
+    public void SetMove(float dir) {
+        m_rigidbody.velocity = transform.forward * maxSpeed * dir;
+    }
+
+    public void SetRotate(float dir) {
+        m_rigidbody.angularVelocity = transform.up * maxTorque * dir;
+    }
+
+}
